f9be0ce98c267adfee21bd5d1ffe2127d87bb380
 Виталя <vitalii@MacBook-Air-Vitala.local>
2022-05-12 18:11:29 +0300

    add task 7

---
+++ Task_7.py
@@ -0,0 +1,14 @@
+# Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+
+def tru_statement(x, y, z):
+    print(f"¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} is {(not (x or y or z)) == (not x and not y and not z)}")
+
+
+tru_statement(0, 0, 0)
+tru_statement(0, 0, 1)
+tru_statement(0, 1, 0)
+tru_statement(0, 1, 1)
+tru_statement(1, 0, 0)
+tru_statement(1, 0, 1)
+tru_statement(1, 1, 0)
+tru_statement(1, 1, 1)
