29b154394d8c17180f079d8c93108b57f567182f
 Виталя <vitalii@MacBook-Air-Vitala.local>
2022-05-14 18:18:28 +0300

    add task 16 did on the 3th lesson

---
+++ Task_16.py
@@ -0,0 +1,22 @@
+# Задать список из n чисел последовательности (1+〖1/n)〗^n и вывести на экран их сумму
+
+# Функция на получение целого числа с проверкой на ввод буквы
+def get_number():
+    while True:
+        try:
+            console_input = abs(int(input('Введите число: ')))
+            return int(console_input)
+        except ValueError:
+            print("Is not a valid number.")
+
+
+# Получаем число
+n = get_number()
+
+# Создаем пустой массив
+massive = []
+
+# Цикл в котором заполняется массив по формуле. Функция append добавляет новый элемент в конец.
+for i in range(1, n+1):
+    massive.append(1+(1/i)**i)
+print(massive)
